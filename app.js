e36ae2b518fc0d21c26dde436bda61f400f1c9a9
tsiMaca <santos.macarenat@gmail.com>
2024-07-17 19:52:03 -0400

    parks data and display logic added

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 let map;
+let markers = [];
+
+const displayPlazasList = () =>{
+    let plazaHtml ="";
+    plazas.forEach(plaza =>{
+        plazaHtml += `<h4> ${plaza.name}</h4>`
+    })
+    document.getElementById("plazas__names").innerHTML = plazaHtml
+}
+
+const sidebar = document.getElementById("sidebar");
+
+const createMarker = (coord, name) => {
+  const marker = new google.maps.marker.AdvancedMarkerElement({ map, position: coord });
+  markers.push(marker);
+};
+
+const createLocationMarkers = () => {
+  plazas.forEach((plaza) => {
+    let coord = new google.maps.LatLng(plaza.lat, plaza.lng);
+    let name = plaza.name;
+    createMarker(coord, name);
+  });
+};
+
 const initMap = async () => {
   const myLatlng = { lat: -27.450752356733336, lng: -58.98624635360478 };
-  const myLatlng2 = { lat: -27.45037835821434, lng: -58.979428779650746 };
-  const myLatlng3 = { lat: -27.44424020133436, lng: -58.98654384774095 };
-  const myLatlng4 = { lat: -27.45117035357736, lng: -58.99417953056948 };
-  const myLatlng5 = { lat: -27.45805607357223, lng: -58.98634551831683 };
 
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  await google.maps.importLibrary("marker");
 
   map = new Map(document.getElementById("map"), {
     center: myLatlng,
@@ -15,32 +36,9 @@ const initMap = async () => {
     mapId: "DEMO_MAP_ID",
   });
 
-  const sidebar = document.getElementById("sidebar");
-
-  const markers = [
-    new AdvancedMarkerElement({ map, position: myLatlng, title: "Click to zoom" }),
-    new AdvancedMarkerElement({ map, position: myLatlng2, title: "Click to zoom" }),
-    new AdvancedMarkerElement({ map, position: myLatlng3, title: "Click to zoom" }),
-    new AdvancedMarkerElement({ map, position: myLatlng4, title: "Click to zoom" }),
-    new AdvancedMarkerElement({ map, position: myLatlng5, title: "Click to zoom" })
-  ];
-
-  map.addListener("center_changed", () => {
-    window.setTimeout(() => {
-      map.panTo(markers[0].position);
-    }, 3000);
-  });
-
-  markers.forEach((marker, index) => {
-    marker.addListener("click", (event) => {
-      console.log(`Marker ${index + 1} clicked`);
-      let latitude = event.latLng.lat();
-      let longitude = event.latLng.lng();
-      console.log(`Latitude: ${latitude} Longitude: ${longitude}`);
-      map.setZoom(8);
-      map.setCenter(marker.position);
-    });
-  });
+  createLocationMarkers();
+  displayPlazasList()
 };
 
 initMap();
+
---
+++ data.js
@@ -0,0 +1,34 @@
+let plazas = [
+  {
+    name: "Plaza 25 de Mayo",
+    lat: -27.450752356733336,
+    lng: -58.98624635360478,
+    description:
+      "Is the park in the center of the city, between the 4 main avenues",
+  },
+  {
+    name: "Plaza General Belgrano",
+    lat: -27.45037835821434,
+    lng: -58.979428779650746,
+    description:
+      " Three blocks away from the main park and in front of the UTN (National Technology University) ",
+  },
+  {
+    name: "Plaza 12 de Octubre",
+    lat: -27.44424020133436,
+    lng: -58.98654384774095,
+    description: " Four blocks away from the main park",
+  },
+  {
+    name: "Plaza 9 de Julio",
+    lat: -27.45117035357736,
+    lng: -58.99417953056948,
+    description: "Four blocks away from the main park",
+  },
+  {
+    name: "Plaza España",
+    lat: -27.45805607357223,
+    lng: -58.98634551831683,
+    description: "Four blocks away from the main park",
+  },
+];
